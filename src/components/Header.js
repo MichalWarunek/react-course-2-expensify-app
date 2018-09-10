6645145e8775b3039a3af2cff76cf8736439bca6
michal992 <tiqer3k@gmail.com>
2018-09-10 11:47:48 +0200

    Store user data in user area

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({ startLogout }) =>
 (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <button onClick={startLogout}>Logout</button>
     
@@ -19,3 +19,5 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(undefined, mapDispatchToProps)(Header);
+
+
